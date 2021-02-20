1755a1e737cde1733edbc23f46799c5e981a414b
cuvillier-ryan <cuvillier.ryan@gmail.com>
2021-02-19 18:04:17 -0600

    next commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function isLeap(year) {
   //this is a test
 
   function isLeap(year) {
-    if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
       return "Leap year."
     }
     return "Not leap year."
