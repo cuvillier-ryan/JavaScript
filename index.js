1aa0cb2a5723ccc5d51a38cde38c30c33dc98b37
cuvillier-ryan <>
2021-02-19 19:19:01 -0600

    leapYear

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ function isLeap(year) {
   }
 }
 
-function isLeapToo(year){
-  if(year % 4 === 0) {
-    if(year % 100 === 0){
-      if(year % 400 === 0) {
+function isLeapToo(year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
         return "Leap year.";
-      }else {
+      } else {
         return "Not leap year.";
       }
-    }else {
+    } else {
       return "Leap year.";
     }
   } else {
