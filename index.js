4472e2aee01921e891cd16843924460151bd7d2b
cuvillier-ryan <cuvillier.ryan@gmail.com>
2021-02-22 15:57:58 -0600

    fizzBuzzWhileLoopAdded

--- index.js
+++ index.js
@@ -34,19 +34,23 @@ var count = 1;
 
 let fizzBuzz = function() {
 
-  if (count % 3 === 0 && count % 5 === 0) {
-    output.push("Fizz Buzz");
-  } else if (count % 3 === 0) {
-    output.push("Fizz");
-  } else if (count % 5 === 0) {
-    output.push("Buzz");
-  } else {
-    output.push(count)
-  }
+  while (count <= 100) {
 
-  count++
+    if (count % 3 === 0 && count % 5 === 0) {
+      output.push("FizzBuzz");
+    } else if (count % 3 === 0) {
+      output.push("Fizz");
+    } else if (count % 5 === 0) {
+      output.push("Buzz");
+    } else {
+      output.push(count)
+    }
+
+    count++
+  }
   console.log(output);
 }
+
 
 //Test
 
