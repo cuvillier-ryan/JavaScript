352e47ecad28ab5a6e978cf17b2d2953203dda94
cuvillier-ryan <cuvillier.ryan@gmail.com>
2021-02-22 16:15:35 -0600

    beerWallAdded

--- index.js
+++ index.js
@@ -67,3 +67,18 @@ function whosPaying(names) {
   return randomPerson + " is going to buy lunch today!"
 
 }
+
+// 99 bottles of beer on the wall
+
+var numberOfBottles = 99
+while (numberOfBottles >= 0) {
+    var bottleWord = "bottle";
+    if (numberOfBottles === 1) {
+        bottleWord = "bottles";
+    }
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+    console.log(numberOfBottles + " " + bottleWord + " of beer,");
+    console.log("Take one down, pass it around,");
+	numberOfBottles--;
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+}
