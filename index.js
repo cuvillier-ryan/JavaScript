434064b3c71a46fd66cf002d2982ae1ef0629167
cuvillier-ryan <cuvillier.ryan@gmail.com>
2021-02-22 12:40:23 -0600

    test

--- index.js
+++ index.js
@@ -29,25 +29,40 @@ function isLeapToo(year) {
 
 //Basic FizzBuzz
 
-var output = [];
-var count = 1;
+// var output = [];
+// var count = 1;
+//
+// let fizzBuzz = function(){
+//
+//     if(count % 3 === 0){
+//         output.push("Fizz");
+//     } else if(count % 5 === 0){
+//         output.push("Buzz");
+//     } else if(count % 3 === 0 && count % 5 === 0){
+//         output.push("Fizz Buzz");
+//     } else {
+//         output.push(count);
+//     }
+//
+//
+//     count++
+//     console.log(output);
+//
+// }
 
-let fizzBuzz = function(){
-
-    if(count % 3 === 0){
-        output.push("Fizz");
-    } else if(count % 5 === 0){
-        output.push("Buzz");
-    } else if(count % 3 === 0 && count % 5 === 0){
-        output.push("Fizz Buzz");
+let fizzBuzz = function() {
+  for (let i = 0; i < 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("Fizz Buzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
     } else {
-        output.push(count);
+      console.log(i);
     }
-
-
-    count++
-    console.log(output);
-
+  }
 }
+
 //Test
 // Test
