90804a715a53e4cd9efbc17820167a6cd6fade73
cuvillier-ryan <cuvillier.ryan@gmail.com>
2021-02-22 09:52:09 -0600

    basic fizzBuzz

--- index.js
+++ index.js
@@ -27,4 +27,27 @@ function isLeapToo(year) {
   }
 }
 
+//Basic FizzBuzz
+
+var output = [];
+var count = 1;
+
+let fizzBuzz = function(){
+
+    if(count % 3 === 0){
+        output.push("Fizz");
+    } else if(count % 5 === 0){
+        output.push("Buzz");
+    } else if(count % 3 === 0 && count % 5 === 0){
+        output.push("Fizz Buzz");
+    } else {
+        output.push(count);
+    }
+
+
+    count++
+    console.log(output);
+
+}
+//Test
 // Test
