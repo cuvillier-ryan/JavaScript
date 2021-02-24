3d62f2a0e7130fefe8377d2ab2f244e532995327
cuvillier-ryan <cuvillier.ryan@gmail.com>
2021-02-24 12:52:23 -0600

    fibonacciSequence

--- index.js
+++ index.js
@@ -103,3 +103,26 @@ while (numberOfBottles >= 0) {
 	numberOfBottles--;
     console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
 }
+
+
+// Fibonacci Sequence Generator
+
+function fibonacciGenerator(n) {
+
+    var output = [];
+    if(n === 1){
+        output = [0];
+    }
+    else if(n === 2){
+        output = [0, 1];
+    }
+    else {
+        output = [0, 1];
+
+        for(var i = 2; i < n; i++){
+            output.push(output[output.length - 2] + output[output.length -1]);
+        }
+
+    }
+    return output;
+}
