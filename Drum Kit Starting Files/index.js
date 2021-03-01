6440c13a098d1611ed79523658ad80f3bf46b2e4
cuvillier-ryan <cuvillier.ryan@gmail.com>
2021-03-01 17:39:32 -0600

    switchStatementAdded

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,14 +1,54 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
-var audio = new Audio("sounds/tom-1.mp3");
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
 
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    this.style.color = "white";
+
+    var buttonInnerHTML = this.innerHTML;
+
+    switch (buttonInnerHTML) {
+      case "w":
+        var kickDrum = new Audio("sounds/kick-bass.mp3");
+        kickDrum.play();
+        break;
+
+      case "a":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+
+      case "s":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+
+      case "d":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+
+      case "j":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+
+      case "k":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+
+      case "l":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+
+      default:
+
+
+    }
+
 
 
   });
 
 }
-
-// audio.play();
